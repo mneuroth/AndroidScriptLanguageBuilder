e96e6fa438beac69a799585e5fa706d2118f98e7
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-13 22:01:59 +0200

    Try to build armv7a binaries

--- build_android_lua.sh
+++ build_android_lua.sh
@@ -2,11 +2,12 @@
 
 export PATH="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH"
 export COMPILER_TARGET=$1
+export TOOLS_TARGET=$2
 export CC=$COMPILER_TARGET"26-clang"
-export LD=$COMPILER_TARGET"-ld"
-export AR=$COMPILER_TARGET"-ar rcu"
-export RANLIB=$COMPILER_TARGET"-ranlib"
-export STRIP=$COMPILER_TARGET"-strip"
+export LD=$TOOLS_TARGET"-ld"
+export AR=$TOOLS_TARGET"-ar rcu"
+export RANLIB=$TOOLS_TARGET"-ranlib"
+export STRIP=$TOOLS_TARGET"-strip"
 export MYCFLAGS="-fPIC"
 export MYLDFLAGS="-pie"
 #echo $COMPILER_TARGET
