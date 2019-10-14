8d4e40137457db42deb00c88017eddff7563ecc8
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-14 20:35:52 +0200

    Fixed path to script to setup build environment

--- build_android_gnuplot.sh
+++ build_android_gnuplot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source ./setup_ndk.sh $1 $2 $3 $4 $5
+source ../setup_ndk.sh $1 $2 $3 $4 $5
 export CFLAGS="-fdata-sections -ffunction-sections -funwind-tables -fstack-protector-strong -no-canonical-prefixes -mstackrealign -fno-addrsig -fPIC "
 ./configure --host=i686-linux-android --without-readline -with-qt=no --without-cairo 
 make -j 4 || true   # ignore build error when cross compiling
