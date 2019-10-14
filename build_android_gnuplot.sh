e194aefc5546057eb1c8f1cab0c1389eb56845f5
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-14 19:07:15 +0200

    Extract setup of android compiler environment into own shell script

--- build_android_gnuplot.sh
+++ build_android_gnuplot.sh
@@ -1,16 +1,5 @@
-#!/bin/sh
-
-export PATH="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH"
-export COMPILER_TARGET=$1
-export TOOLS_TARGET=$2
-export VERSION_NO=$3
-export CC=$COMPILER_TARGET"26-clang"
-export LD=$TOOLS_TARGET"-ld"
-export AR=$TOOLS_TARGET"-ar rcu"
-export RANLIB=$TOOLS_TARGET"-ranlib"
-export STRIP=$TOOLS_TARGET"-strip"
-export MYCFLAGS="-fPIC"
-export MYLDFLAGS="-pie"
+#!/bin/bash
+. setup_ndk.sh $1 $2 $3 $4 $5
 export CFLAGS="-fdata-sections -ffunction-sections -funwind-tables -fstack-protector-strong -no-canonical-prefixes -mstackrealign -fno-addrsig -fPIC "
 ./configure --host=i686-linux-android --without-readline -with-qt=no --without-cairo 
 make -j 4 || true   # ignore build error when cross compiling
