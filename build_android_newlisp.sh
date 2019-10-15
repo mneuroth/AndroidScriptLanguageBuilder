1fff3df62614e9e82479fdac9b59cda890447c52
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-15 07:55:06 +0200

    Fixed build script for newlisp

--- build_android_newlisp.sh
+++ build_android_newlisp.sh
@@ -3,14 +3,12 @@
 echo "building newlisp..."
 . ../setup_ndk.sh $1 $2 $3
 cat configure
-./configure --help
-./configure --host=i686-linux-android # --without-readline -with-qt=no --without-cairo 
+cat makefile_linux
+./configure
 make -j 4 
 ls -lrt 
-cd src
-ls -lrt
 $STRIP newlisp
-zip -u ../newlisp-$VERSION_NO-android-$COMPILER_TARGET-bin.zip newlisp
+zip -u newlisp-$VERSION_NO-android-$COMPILER_TARGET-bin.zip newlisp
 ls -lrt
 #- ./newlisp -v
 cd ..
