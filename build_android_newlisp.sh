2a4ac5e839db2466b7ead6518d084a9c77b963d0
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-16 21:42:27 +0200

    Cleanup build script for newlisp and patch sources only for android build

--- build_android_newlisp.sh
+++ build_android_newlisp.sh
@@ -3,13 +3,13 @@
 echo "building newLISP..."
 . ../setup_ndk.sh $1 $2 $3
 cp ../newlisp.patch/makefile_android .
-ls -lrt
+#ls -lrt
 #cat configure
 #cat makefile_linux
 #./configure
 make -f makefile_android
-ls -lrt 
-$STRIP newlisp
+#ls -lrt 
+#$STRIP newlisp
 zip -u newlisp-$VERSION_NO-android-$COMPILER_TARGET-bin.zip newlisp
 ls -lrt
 #- ./newlisp -v
