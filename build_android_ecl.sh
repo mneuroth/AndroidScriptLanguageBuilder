541130de6f9e5e26da90c00f99dec02b6f54802b
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-17 21:47:06 +0200

    Update ecl build script

--- build_android_ecl.sh
+++ build_android_ecl.sh
@@ -2,7 +2,7 @@
 #set -x
 echo "building ECL..."
 . ../setup_ndk.sh $1 $2 $3
-./configure --host=arm-linux-androideabi --prefix==`pwd`/ecl-android --disable-c99complex --with-cross-config=`pwd`/src/util/android-arm.cross_config
+./configure --host=arm-linux-androideabi --prefix=`pwd`/ecl-android --disable-c99complex --with-cross-config=`pwd`/src/util/android-arm.cross_config
 make 
 ls -lrt
 ls -lrt build
