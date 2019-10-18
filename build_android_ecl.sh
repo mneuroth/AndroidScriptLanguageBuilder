dd22a56e6ed8bb01eeeb3db3346428cea896c623
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-18 21:54:20 +0200

    Update ecl build

--- build_android_ecl.sh
+++ build_android_ecl.sh
@@ -4,15 +4,15 @@ echo "building ECL..."
 . ../setup_ndk.sh $1 $2 $3
 export LDFLAGS="--sysroot=${SYSROOT} -D__ANDROID_API__=26 -fuse-ld=bfd"
 export CPPFLAGS="--sysroot=${SYSROOT} -D__ANDROID_API__=26 -isystem ${SYSROOT}/usr/include/arm-linux-androideabi"
-./configure --host=$2 --prefix=`pwd`/ecl-android --disable-c99complex  --disable-gmp --with-cross-config=`pwd`/src/util/android-arm.cross_config
-make 
+./configure --host=$2 --prefix=`pwd`/ecl-android --disable-c99complex  --enable-gmp=include --with-cross-config=`pwd`/src/util/android-arm.cross_config
+make -j 4
 ls -lrt
 ls -lrt build
-ls -lrt src
-$STRIP build/ecl
-cd build
-zip -u ecl-$VERSION_NO-android-$COMPILER_TARGET-bin.zip ecl
-ls -lrt
+ls -lrt build/bin
+ls -lrt build/ecl
+cd build/bin
+$STRIP ecl
+zip -u ../../../ecl-$VERSION_NO-android-$COMPILER_TARGET-bin.zip ecl *.so
 #- ./ecl -v
 cd ..
 cd ..
