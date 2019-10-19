436f655a2277d79ceab42a3fcc188f7c39e0de9d
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-19 15:37:39 +0200

    Try to zip binara of ecl into zip file

--- build_android_ecl.sh
+++ build_android_ecl.sh
@@ -5,13 +5,13 @@ echo "building ECL..."
 export AR=$2"-ar"
 export LDFLAGS="--sysroot=${SYSROOT} -D__ANDROID_API__=26 -fuse-ld=bfd"
 export CPPFLAGS="--sysroot=${SYSROOT} -D__ANDROID_API__=26 -isystem ${SYSROOT}/usr/include/arm-linux-androideabi"
-./configure --host=$2 --prefix=`pwd`/ecl-android --disable-c99complex  --enable-gmp=include --with-cross-config=`pwd`/src/util/android-arm.cross_config
+./configure --host=$2 --prefix=`pwd`/ecl-android --disable-c99complex --with-cross-config=`pwd`/src/util/android-arm.cross_config
 make -j 4
 ls -lrt
 ls -lrt build
 ls -lrt build/bin
 ls -lrt build/ecl
-cd build/bin
+cd build/ecl
 $STRIP ecl
 zip -u ../../../ecl-$VERSION_NO-android-$COMPILER_TARGET-bin.zip ecl *.so
 #- ./ecl -v
