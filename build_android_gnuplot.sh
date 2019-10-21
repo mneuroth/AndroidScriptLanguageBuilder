f6cb0b6e4b1bf1a9fc049800426d3800e0f9bc2d
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-21 23:03:53 +0200

    Update build scripts

--- build_android_gnuplot.sh
+++ build_android_gnuplot.sh
@@ -3,7 +3,7 @@
 echo "building gnuplot..."
 . ../setup_ndk.sh $1 $2 $3
 export CFLAGS="-fdata-sections -ffunction-sections -funwind-tables -fstack-protector-strong -no-canonical-prefixes -mstackrealign -fno-addrsig -fPIC "
-./configure --host=i686-linux-android --without-readline -with-qt=no --without-cairo 
+./configure --host=$2 --without-readline -with-qt=no --without-cairo 
 make -j 4 || true   # ignore build error when cross compiling
 ls -lrt 
 cd src
--- build_android_hugs.sh
+++ build_android_hugs.sh
@@ -4,14 +4,13 @@ echo "building hugs98..."
 . ../setup_ndk.sh $1 $2 $3
 #export CFLAGS="-fdata-sections -ffunction-sections -funwind-tables -fstack-protector-strong -no-canonical-prefixes -mstackrealign -fno-addrsig -fPIC "
 ./configure --help
-./configure --host=i686-linux-android --without-readline 
+./configure --host=$2 # --without-readline 
 make -j 4 || true   # ignore build error when cross compiling
 ls -lrt 
 cd src
 ls -lrt
 $STRIP hugs
 $STRIP runhugs
-cp ../../gnuplot.gih .
 zip -u ../../hugs98-plus-$VERSION_NO-android-$COMPILER_TARGET-bin.zip hugs runhugs
 ls -lrt
 #- ./hugs -v
