340ff7dec4e718d5acdaba3a0ec13776b3e5672f
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-21 23:16:20 +0200

    Try to configure for cross compiling

--- build_android_hugs.sh
+++ build_android_hugs.sh
@@ -4,7 +4,8 @@ echo "building hugs98..."
 . ../setup_ndk.sh $1 $2 $3
 #export CFLAGS="-fdata-sections -ffunction-sections -funwind-tables -fstack-protector-strong -no-canonical-prefixes -mstackrealign -fno-addrsig -fPIC "
 ./configure --help
-./configure --host=$2 # --without-readline 
+./configure --host=$2 --build=i386-linux # --without-readline 
+cat src/Makefile
 make -j 4 || true   # ignore build error when cross compiling
 ls -lrt 
 cd src
