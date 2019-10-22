0864552a01c6667c3e8f879147ab4c52a0a8225a
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-22 16:43:29 +0200

    Try to cross compile hugs

--- build_android_hugs.sh
+++ build_android_hugs.sh
@@ -3,9 +3,8 @@
 echo "building hugs98..."
 . ../setup_ndk.sh $1 $2 $3
 #export CFLAGS="-fdata-sections -ffunction-sections -funwind-tables -fstack-protector-strong -no-canonical-prefixes -mstackrealign -fno-addrsig -fPIC "
-./configure --help
-./configure --help --host
-./configure --host=arm-linux-eabi --build=i686-pc-linux # --without-readline 
+#./configure --help
+#./configure --host=arm-linux-eabi --build=i686-pc-linux # --without-readline 
 cat src/Makefile
 # after building for the host, patch sources for android
 sed -i 's/CC		= gcc/#CC		= gcc/g' src/Makefile
