8082731d5e7b2fb42c5d8661a9448d3f4a77287a
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-22 19:57:37 +0200

    Another try

--- build_android_hugs.sh
+++ build_android_hugs.sh
@@ -5,14 +5,19 @@ echo "building hugs98..."
 #export CFLAGS="-fdata-sections -ffunction-sections -funwind-tables -fstack-protector-strong -no-canonical-prefixes -mstackrealign -fno-addrsig -fPIC "
 #./configure --help
 #./configure --host=arm-linux-eabi --build=i686-pc-linux # --without-readline 
+echo "============================"
 cat src/Makefile
+echo "============================"
 cat src/config.h
 # after building for the host, patch sources for android
 sed -i 's/CC		= gcc/#CC		= gcc/g' src/Makefile
 sed -i 's/CC		= gcc/#CC		= gcc/g' src/Makefile
 sed -i 's/LD		= ld/#LD		= ld/g' src/Makefile
 sed -i 's/#define HAVE_FTIME 1/#define HAVE_FTIME 0/g' src/config.h
-make -j 4 || true   # ignore build error when cross compiling
+sed -i 's/#define HAVE_SYS_TIMEB_H 1/#define HAVE_SYS_TIMEB_H 0/g' src/config.h
+echo "============================"
+cat src/config.h
+make -j 4 # || true   # ignore build error when cross compiling
 ls -lrt 
 cd src
 ls -lrt
