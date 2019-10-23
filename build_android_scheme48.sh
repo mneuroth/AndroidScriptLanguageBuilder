52c90d45e4bc3d44a4928cd7c89ec7d1f13f95bb
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-23 20:08:18 +0200

    Fixed hugs build and improved scheme48 build

--- build_android_scheme48.sh
+++ build_android_scheme48.sh
@@ -3,13 +3,15 @@
 echo "building scheme48..."
 . ../setup_ndk.sh $1 $2 $3
 ./configure --help
-./configure --host=arm-linux-eabi --build=i686-pc-linux # --without-readline 
 echo "============================"
-cat src/Makefile
+#./configure --host=arm-linux-eabi --build=i686-pc-linux # --without-readline 
+./configure --host=$2 --build=i686-pc-linux
+echo "============================"
+cat Makefile
 make -j 4 # || true   # ignore build error when cross compiling
 ls -lrt
 $STRIP scheme48
-zip -u ../../scheme48-$VERSION_NO-android-$COMPILER_TARGET-bin.zip scheme48 scheme48vm scheme48.image
+zip -u ../scheme48-$VERSION_NO-android-$COMPILER_TARGET-bin.zip scheme48vm scheme48.image
 ls -lrt
 #- ./scheme48
 cd .. 
