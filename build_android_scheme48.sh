72606cecadeb2c7ef3746b2f5ee56b03f9a1370b
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-23 20:33:01 +0200

    Update sceme48 build script

--- build_android_scheme48.sh
+++ build_android_scheme48.sh
@@ -9,10 +9,12 @@ echo "============================"
 echo "============================"
 sed -i 's/CC = gcc/#CC = gcc/g' Makefile
 sed -i 's/LD = gcc/#LD = gcc/g' Makefile
+sed -i 's/-shared  -rdynamic/-shared /g' Makefile
 cat Makefile
 make -j 4 # || true   # ignore build error when cross compiling
 ls -lrt
-$STRIP scheme48
+$STRIP scheme48vm
+ls -lrt
 zip -u ../scheme48-$VERSION_NO-android-$COMPILER_TARGET-bin.zip scheme48vm scheme48.image
 ls -lrt
 #- ./scheme48
