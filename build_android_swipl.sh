ec537a7eb11dbee0ed4ca78b5afd67e213e2e9f0
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-25 21:26:16 +0200

    Improve cross compiling for prolog

--- build_android_swipl.sh
+++ build_android_swipl.sh
@@ -5,13 +5,14 @@ echo "building swipl..."
 #export AR=$2"-ar"
 #./configure --host=$2 --build=i686-pc-linux # --disable-shared
 #echo "============================"
+cmake  -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_ABI=$1 -DINSTALL_DOCUMENTATION=OFF -DUSE_GMP=ON -DSWIPL_INSTALL_IN_LIB=ON -DSWIPL_PACKAGES_BDB=OFF -DSWIPL_PACKAGES_ODBC=OFF -DSWIPL_PACKAGES_QT=OFF -DSWIPL_PACKAGES_X=OFF ..
 make -j 4 || true   # ignore build error when cross compiling
 ls -lrt
 cd src
 ls -lrt 
 $STRIP swipl
 ls -lrt
-zip -u ../../swipl-$lVERSION_NO-android-$COMPILER_TARGET-bin.zip swipl swipl.so.$3
+zip -u ../../swipl-$VERSION_NO-android-$COMPILER_TARGET-bin.zip swipl libswipl.so.$3
 ls -lrt
 #./swipl
 cd ..
