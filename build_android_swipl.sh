f216be32a90ea232052837f14b3a8682ae5772ba
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-29 20:07:29 +0100

    Cleaned temporaray cmake files

--- build_android_swipl.sh
+++ build_android_swipl.sh
@@ -6,7 +6,7 @@ echo "building swipl..."
 #./configure --host=$2 --build=i686-pc-linux # --disable-shared
 #echo "============================"
 cmake  -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_ABI=$1 -DINSTALL_DOCUMENTATION=OFF -DUSE_GMP=ON -DSWIPL_INSTALL_IN_LIB=ON -DSWIPL_PACKAGES_BDB=OFF -DSWIPL_PACKAGES_ODBC=OFF -DSWIPL_PACKAGES_QT=OFF -DSWIPL_PACKAGES_X=OFF ..
-make -j 4 || true   # ignore build error when cross compiling
+make -j 4 # || true   # ignore build error when cross compiling
 ls -lrt
 cd src
 ls -lrt 
