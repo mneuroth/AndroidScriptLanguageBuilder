885b26cace42cb267369f396cada350ebefcb7de
Michael Neuroth <michael.neuroth@freenet.de>
2019-10-29 21:19:05 +0100

    Try to build prolog for android

--- build_android_swipl.sh
+++ build_android_swipl.sh
@@ -5,8 +5,9 @@ echo "building swipl..."
 #export AR=$2"-ar"
 #./configure --host=$2 --build=i686-pc-linux # --disable-shared
 #echo "============================"
-cmake  -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_ABI=$4 -DINSTALL_DOCUMENTATION=OFF -DUSE_GMP=ON -DSWIPL_INSTALL_IN_LIB=ON -DSWIPL_PACKAGES_BDB=OFF -DSWIPL_PACKAGES_ODBC=OFF -DSWIPL_PACKAGES_QT=OFF -DSWIPL_PACKAGES_X=OFF ..
-make -j 4 # || true   # ignore build error when cross compiling
+cmake -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_ABI=$4 -DINSTALL_DOCUMENTATION=OFF -DSWIPL_PACKAGES=ON -DUSE_GMP=OFF -DSWIPL_SHARED_LIB=OFF -DSWIPL_PACKAGES_BDB=OFF -DSWIPL_PACKAGES_ODBC=OFF -DSWIPL_PACKAGES_QT=OFF -DSWIPL_PACKAGES_X=OFF ..
+ninja 
+#make -j 4 # || true   # ignore build error when cross compiling
 ls -lrt
 cd src
 ls -lrt 
@@ -14,5 +15,6 @@ $STRIP swipl
 ls -lrt
 zip -u ../../swipl-$VERSION_NO-android-$COMPILER_TARGET-bin.zip swipl libswipl.so.$3
 ls -lrt
+ninja clean
 #./swipl
 cd ..
