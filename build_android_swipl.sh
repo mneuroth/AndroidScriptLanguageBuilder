dcd6c7807fc3bb47d6ac94c5c5dd1edc7687c11c
Michael Neuroth <michael.neuroth@freenet.de>
2020-05-07 23:45:12 +0200

    Try to build swi-prolog

--- build_android_swipl.sh
+++ build_android_swipl.sh
@@ -5,9 +5,10 @@ echo "building swipl..."
 #export AR=$2"-ar"
 #./configure --host=$2 --build=i686-pc-linux # --disable-shared
 #echo "============================"
-cmake -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_ABI=$4 -DINSTALL_DOCUMENTATION=OFF -DSWIPL_PACKAGES=ON -DUSE_GMP=OFF -DSWIPL_SHARED_LIB=OFF -DSWIPL_PACKAGES_BDB=OFF -DSWIPL_PACKAGES_ODBC=OFF -DSWIPL_PACKAGES_QT=OFF -DSWIPL_PACKAGES_X=OFF ..
-ninja 
-#make -j 4 # || true   # ignore build error when cross compiling
+#cmake -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_ABI=$4 -DINSTALL_DOCUMENTATION=OFF -DSWIPL_PACKAGES=ON -DUSE_GMP=OFF -DSWIPL_SHARED_LIB=OFF -DSWIPL_PACKAGES_BDB=OFF -DSWIPL_PACKAGES_ODBC=OFF -DSWIPL_PACKAGES_QT=OFF -DSWIPL_PACKAGES_X=OFF ..
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_ABI=$4 -DINSTALL_DOCUMENTATION=OFF -DSWIPL_PACKAGES=ON -DUSE_GMP=OFF -DSWIPL_SHARED_LIB=OFF -DSWIPL_PACKAGES_BDB=OFF -DSWIPL_PACKAGES_ODBC=OFF -DSWIPL_PACKAGES_QT=OFF -DSWIPL_PACKAGES_X=OFF ..
+#ninja 
+make -j 4 || true   # ignore build error when cross compiling
 ls -lrt
 cd src
 ls -lrt 
